7071a7a3d783271d784d5c5ba44b9d396c3991e2
Russ <russlascala@gmail.com>
2018-11-10 12:53:29 -0500

    Added content to page waiting for revies

--- js/script.js
+++ js/script.js
@@ -115,3 +115,21 @@ function fixStepIndicator(n) {
     //... and adds the "active" class to the current step:
     x[n].className += " active";
 }
+
+/* Script to change the column stacking for mobile to get list on bottom of form */
+$(window).resize(function () {
+    /*  console.log('resize called'); */
+    var width = $(window).width();
+    /*  console.log(width); */
+    /*  log the width to find out what what pixel to change the order at.
+        The dev tools in browser might be different */
+    //console.log(width); 
+    if (width <= 767) { //991 752
+        $('.topMobile').addClass('order-first');
+        $('.bottomMobile').addClass('order-last');
+    } else {
+        $('.topMobile').removeClass('order-first');
+        $('.bottomMobile').removeClass('order-last');
+    }
+})
+    .resize();//trigger the resize event on page load.
