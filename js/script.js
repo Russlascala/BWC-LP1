527c2d884e29bd8a1de51a4c5a5f66c7f7a2afff
Russ <russlascala@gmail.com>
2018-11-12 23:03:27 -0500

    added script for buttons and added buttons every three paragraphs

--- js/script.js
+++ js/script.js
@@ -133,3 +133,14 @@ $(window).resize(function () {
     }
 })
     .resize();//trigger the resize event on page load.
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function () {
+            if (window.scrollY != 0) {
+                window.scrollBy(0, scrollStep);
+            }
+            else clearInterval(scrollInterval);
+        }, 15);
+}
